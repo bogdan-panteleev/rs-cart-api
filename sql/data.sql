bfc21be905dbca5c512908396292dbfdc6764f57
panteleev bogdan <panteleevbbbogdan@gmail.com>
2022-11-20 14:33:44 +0300

    add SQL scripts

---
+++ sql/data.sql
@@ -0,0 +1,10 @@
+--insert into carts (created_at, updated_at) values 
+--('2022-11-19', '2022-11-19'),
+--('2022-11-17', '2022-11-17')
+--;
+
+--insert into cart_items (cart_id, items_count) values
+--('11a3ac49-a306-448b-b994-b66cb998c764', 10),
+--('33b5bb8a-8dc7-4b1e-b9c7-9ea7042f3710', 5),
+--('6bc6ba04-7bac-4a24-ad6d-0edeff2fd295', 50)
+--;
